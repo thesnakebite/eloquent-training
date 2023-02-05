f76f1d99332eb42cf0eae8da1874b60d6de73a5b
thesnakebite <alvaro@thesnakebite.es>
2023-02-05 20:41:24 +0100

    First Test

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('has-one', function () {
+    $user = User::first();
+
+    return $user->profile;
+    
 });
---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use App\Models\UserProfile;
+use PHPUnit\Framework\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function can_get_the_user_profile_associated_to_a_user()
+    {
+        $this->markTestIncomplete();
+
+        $user = User::factory()->create();
+        $userProfile = UserProfile::factory()->create([
+            'website' => 'https://thesnakebite.es',
+            'user_id' => $user->id,
+        ]);
+
+        $this->assertInstanceOf(UserProfile::class, $user->profile);
+        $this->assertTrue($userProfile->is($user->profile));
+        $this->assertSame('https://thesnakebite.es', $user->profile->website);
+    }
+}
